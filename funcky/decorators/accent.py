fb8ec681915dc29bbffef55e27c086e734e99708
Dan Ballance <work@danballance.uk>
2024-12-10 23:09:11 +0000

    Add Accent decorator

---
+++ funcky/decorators/accent.py
@@ -0,0 +1,33 @@
+from itertools import cycle
+from typing import Generator
+
+from funcky.decorators.base_decorator import BaseDecorator
+from funcky.sequences.note_sequence import NoteSequence
+
+
+class Accent(BaseDecorator):
+    _accent_generator: Generator[int]
+
+    def __init__(
+        self,
+        cycle_length: int,
+        accent_pattern: list[int],
+    ):
+        self._accent_generator = self._make_accent_generator(
+            cycle_length=cycle_length,
+            accent_pattern=accent_pattern,
+        )
+
+    def _decorate(self, seq: NoteSequence, i: int) -> NoteSequence:
+        if seq[i] is not None:
+            seq[i].velocity += self._accent_generator.__next__()
+        return seq
+
+    @staticmethod
+    def _make_accent_generator(
+        cycle_length: int,
+        accent_pattern: list[int],
+    ) -> Generator[int]:
+        for accent in cycle(accent_pattern):
+            for _ in range(cycle_length):
+                yield accent
--- tracks/hello_world/tracks.py
+++ tracks/hello_world/tracks.py
@@ -1,20 +1,25 @@
+from funcky.decorators.accent import Accent
 from funcky.decorators.repetition import Repetition
 from funcky.decorators.transpose import Transpose
 from funcky.decorators.tune import Tune
 
 from funcky.named_constants import Pitch, Tone, Mode
-from funcky.rhythm import steps_8
+from funcky.rhythm import steps_8, steps_16
 from funcky.scales import scale_generator
 from funcky.sequences.note_sequence import NoteSequence
 
 
+@Accent(
+    cycle_length=1,
+    accent_pattern=[50, 0, 0, 25, 50, 0, 25, 0]
+)
 @Transpose(
     cycle_length=8,
     transposition_pattern=[0, 0, 2, 1]
 )
 @Tune(
     scale_generator=scale_generator(
-        root=Pitch(tone=Tone.C, octave=4, accidental=0),
+        root=Pitch(tone=Tone.C, octave=3, accidental=0),
         mode=Mode.Minor,
         notes=lambda: [1, 3, 4, 5],
     )
